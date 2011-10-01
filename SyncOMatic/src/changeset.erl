7af18b41fe29168b8f14584ddf87bd2027a1cb08
Lapinferoce <buk.rogers@gmail.com>
2011-10-01 19:10:17 +0300

    Edited SyncOMatic/src/changeset.erl via GitHub

--- SyncOMatic/src/changeset.erl
+++ SyncOMatic/src/changeset.erl
@@ -2,6 +2,7 @@
 -export([diff/2,display_CS/1,isIn/2]).
 -import(fileset).
 -import(misc).
+%-import(lists:sort/2).
 
 %CS display
 display_CS({A,C,R})->
@@ -25,15 +26,15 @@ display_CS({A,C,R})->
 %return a change set
 % using the new way avoiding lookup again and again
  diff(FS1,FS2)->
-     sorted_FS1 = sort(fun({PA,HA}, {PB,HB}) -> PA >= PB end,FS1),
-     sorted_FS2 = sort(fun({PA,HA}, {PB,HB}) -> PA >= PB end,FS2),
-     P = dodiff(FS1,FS2)
+     sorted_FS1 = lists:sort(fun({PA,HA}, {PB,HB}) -> PA >= PB end,FS1),
+     sorted_FS2 = lists:sort(fun({PA,HA}, {PB,HB}) -> PA >= PB end,FS2),
+     P = dodiff(FS1,FS2),
  %    Added = utilmisc:filterThree(fun(X,Y) ->not isIn(X,Y) end,FS1,FS2),
  %    Changed = utilmisc:filterThree(fun(X,Y) ->hasChangeIn(X,Y) end,FS1,FS2),
  %    Removed = utilmisc:filterThree(fun(X,Y) ->not isIn(X,Y) end,FS2,FS1),
-     {Added,Changed,Removed}.
+     P.
 
-dofiff(FS1,FS2)->
+dodiff(FS1,FS2)->
 	dodiff(FS1,FS2,[],[],[]).
 	
 dodiff(FS1,FS2,Added,Changed,Removed)->
@@ -41,16 +42,17 @@ dodiff(FS1,FS2,Added,Changed,Removed)->
 	[EFS2|TFS2] = FS2,
 	{PEFS1,HEFS1} = EFS1,
 	{PEFS2,HEFS2} = EFS2,
-	case {PEFS1,PEFS2} of
+	if %{PEFS1,PEFS2} of
 		PEFS1 < PEFS2 -> 
 			dodiff(TFS1,FS2,Added++EFS1,Changed,Removed);
 		PEFS1 > PEFS2 ->
-			dodiff(FS1,TFS2,Added,Changed,Removed++ESF2);
+			dodiff(FS1,TFS2,Added,Changed,Removed++EFS2);
 		PEFS1 == PEFS2 ->
-			case {HEFS1==HEFS2} ->
-				dodiff(TFS1,TFS2,Added,Changed,Removed);
-			_ -> 
-				dodiff(TFS1,TFS2,Added,Changed++EFS1,Removed)
+			if %{HEFS1,HEFS2} of
+			    HEFS1 == HEFS2 ->
+				    dodiff(TFS1,TFS2,Added,Changed,Removed);
+			    true -> 
+				    dodiff(TFS1,TFS2,Added,Changed++EFS1,Removed)
 			end
 	end;
 				
